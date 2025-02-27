f610098364ab980aec81d09655c4730b2cd09e28
JunAn <k36887864@gmail.com>
2025-02-27 19:41:23 +0800

    Implement q_new and q_free
    Init empty queue using Check INIT_LIST_HEAD and free storage using list_del for safe node deletion.
    
    Change-Id: I179159f696691ca17950b43ae9d14dbcb8d5acc2

--- queue.c
+++ queue.c
@@ -13,11 +13,25 @@
 /* Create an empty queue */
 struct list_head *q_new()
 {
-    return NULL;
+    struct list_head *new = malloc(sizeof(struct list_head));
+    if (!new)
+        return NULL;
+    INIT_LIST_HEAD(new);
+    return new;
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *head) {}
+void q_free(struct list_head *head)
+{
+    if (!head)
+        return;
+    while (list_empty(head) == false) {
+        struct list_head *n = head->next;
+        list_del(n);
+        free(n);
+    }
+    free(head);
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
